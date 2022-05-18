4e2f1e93921da06b74323dbeacda2663d49c35bf
Onyi <consultonyinye@gmail.com>
2022-05-18 20:32:49 +0000

    Modified the file

--- example2.sh
+++ example2.sh
@@ -1,4 +1,7 @@
 This is the second example of my git example
 do well to add to it if you wish to
 add your own example to corroborate this
-thanks! 
+ 
+
+Just added this line to this file.
+Take note
