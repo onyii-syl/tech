4260e1b142a7333cb22e506100c87637371ccce5
Onyi <consultonyinye@gmail.com>
2022-05-18 20:46:39 +0000

    Modified this file in my branch2

--- example2.sh
+++ example2.sh
@@ -5,3 +5,6 @@ add your own example to corroborate this
 
 Just added this line to this file.
 Take note
+
+This is just me testing my new branch2
+This line is not supposed to appear in the master branch
