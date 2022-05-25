1abf5f6e31af3feba65ad848fe2a63051d44e822
nkiru <access.nkiru@gmail.com>
2022-05-25 12:51:19 +0000

    adding more texts to file

--- example2.sh
+++ example2.sh
@@ -8,3 +8,8 @@ Take note
 
 This is just me testing my new branch2
 This line is not supposed to appear in the master branch
+
+i love my mummy, i love my daddy 
+i love my family. mummy cares for me daddy provides for me
+i love my amily
+
